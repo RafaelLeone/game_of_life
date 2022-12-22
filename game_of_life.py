ddf921436ff1a6d8e8bd39296c631fe89e8dd9d7
RafaelLeone <rinaciodacosta@gmail.com>
2022-12-22 00:18:42 -0300

    beta

--- game_of_life.py
+++ game_of_life.py
@@ -1,4 +1,6 @@
 import numpy
+from time import sleep
+import copy
 
 
 # print('linhacoluna')
@@ -17,97 +19,145 @@ import numpy
 # [2][0]: 1, [2][1]: 1, [2][2]: 0, [2][3]: 0
 # [3][0]: 0, [3][1]: 0, [3][2]: 0, [3][3]: 0
 
-TAMANHO_DA_MATRIZ = 4
+while True:
+    print('\n')
+    input_do_usuario = input("Insira um número inteiro positivo e maior que zero (que será a largura e altura da matriz:) ")
+    try:
+        input_do_usuario = int(input_do_usuario)
+        if int(input_do_usuario) > 0:
+            TAMANHO_DA_MATRIZ = int(input_do_usuario)
+            break
+        else:
+            print("Esse não é um número inteiro positivo maior que zero.")
+    except:
+        print("Esse não é um número inteiro.")
+        continue
 
 matriz = numpy.zeros((TAMANHO_DA_MATRIZ, TAMANHO_DA_MATRIZ), dtype=numpy.int)
 
-# Matriz inicial:
-## Pode colocar os números que quiser entre 0 e TAMANHO_DA_MATRIZ-1
-matriz[1][0] = 1
-matriz[1][1] = 1
-matriz[2][1] = 1
+while True:
+    print('\n')
+    print("Matriz atual: ")
+    print(matriz)
+    print('\n')
+    input_do_usuario = input("Deseja inserir uma célula viva? s/n: ")
+    print('\n')
+    if input_do_usuario == 's':
+        print("Número de linhas: ", TAMANHO_DA_MATRIZ)
+        input_do_usuario = input("Insira o número da linha em que deseja inserir: ")
+        try:
+            linha = int(input_do_usuario) - 1
+        except:
+            print("Entrada inválida.")
+            continue
+        if linha not in range(TAMANHO_DA_MATRIZ):
+            print("Não é um número válido.")
+            continue
+        print("Número de colunas: ", TAMANHO_DA_MATRIZ)
+        input_do_usuario = input("Agora o número da coluna dessa linha: ")
+        try:
+            coluna = int(input_do_usuario) - 1
+        except:
+            print("Entrada inválida.")
+            continue
+        if coluna not in range(TAMANHO_DA_MATRIZ):
+            print("Não é um número válido.")
+            continue
+        matriz[linha][coluna] = 1
+    elif input_do_usuario == 'n':
+        break
+    else:
+        print("Entrada inválida.")
+
+# Matriz inicial padrão do site game of life:
+# matriz[1][2] = 1
+# matriz[2][3] = 1
+# matriz[3][1] = 1
+# matriz[3][2] = 1
+# matriz[3][3] = 1
+
 print('\n')
 print("tempo 0: ")
 print(matriz)
+nova_matriz = copy.deepcopy(matriz)
 
-#Dá pra fazer um for i in range NUMERO_DE_VEZES_QUE_QUERO_RODAR aqui...
-#Ou se o último tempo for igual ao penúltimo                     parar o programa
-#Ou se tudo ficar 0                                              parar o programa
-#Ou se o último  tempo repetir algum que já foi (guardar todes)  parar o programa
-for numero_da_linha, linha in enumerate(matriz):
-    for numero_da_coluna, celula in enumerate(linha):
-        vizinhos = []
-        numero_de_vizinhos_vivos = 0
+for i in range(1000):
+    for numero_da_linha, linha in enumerate(matriz):
+        for numero_da_coluna, celula in enumerate(linha):
+            vizinhos = []
+            numero_de_vizinhos_vivos = 0
 
-        # Se ele for da ponta:
-        if numero_da_linha == 0 and numero_da_coluna == 0:
-            vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna+1]) #Direita
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-0]) #S
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna+1]) #S direita
-        elif numero_da_linha == 0 and numero_da_coluna == TAMANHO_DA_MATRIZ-1:
-            vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna-1]) #Esquerda
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-1]) #S esquerda
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-0]) #S
-        elif numero_da_linha == TAMANHO_DA_MATRIZ-1 and numero_da_coluna == 0:
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-0]) #N
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna+1]) #N direita
-            vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna+1]) #Direita
-        elif numero_da_linha == TAMANHO_DA_MATRIZ-1 and numero_da_coluna == TAMANHO_DA_MATRIZ-1:
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-1]) #N esquerda
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-0]) #N
-            vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna-1]) #Esquerda
+            # Se ele for da ponta:
+            if numero_da_linha == 0 and numero_da_coluna == 0:
+                vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna+1]) #Direita
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-0]) #S
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna+1]) #S direita
+            elif numero_da_linha == 0 and numero_da_coluna == TAMANHO_DA_MATRIZ-1:
+                vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna-1]) #Esquerda
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-1]) #S esquerda
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-0]) #S
+            elif numero_da_linha == TAMANHO_DA_MATRIZ-1 and numero_da_coluna == 0:
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-0]) #N
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna+1]) #N direita
+                vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna+1]) #Direita
+            elif numero_da_linha == TAMANHO_DA_MATRIZ-1 and numero_da_coluna == TAMANHO_DA_MATRIZ-1:
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-1]) #N esquerda
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-0]) #N
+                vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna-1]) #Esquerda
 
-        # Se ele for da parede e nao da ponta.
-        elif numero_da_linha == 0:
-            vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna-1]) #Esquerda
-            vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna+1]) #Direita
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-1]) #S esquerda
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-0]) #S
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna+1]) #S direita
-        elif numero_da_coluna == 0:
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-0]) #N
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna+1]) #N direita
-            vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna+1]) #Direita
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-0]) #S
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna+1]) #S direita
-        elif numero_da_linha == TAMANHO_DA_MATRIZ-1:
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-1]) #N esquerda
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-0]) #N
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna+1]) #N direita
-            vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna-1]) #Esquerda
-            vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna+1]) #Direita
-        elif numero_da_coluna == TAMANHO_DA_MATRIZ-1:
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-1]) #N esquerda
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-0]) #N
-            vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna-1]) #Esquerda
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-1]) #S esquerda
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-0]) #S
-        # Se não:
-        else:
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-1]) #N esquerda
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-0]) #N
-            vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna+1]) #N direita
-            vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna-1]) #Esquerda
-            vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna+1]) #Direita
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-1]) #S esquerda
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-0]) #S
-            vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna+1]) #S direita
+            # Se ele for da parede e nao da ponta.
+            elif numero_da_linha == 0:
+                vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna-1]) #Esquerda
+                vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna+1]) #Direita
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-1]) #S esquerda
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-0]) #S
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna+1]) #S direita
+            elif numero_da_coluna == 0:
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-0]) #N
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna+1]) #N direita
+                vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna+1]) #Direita
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-0]) #S
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna+1]) #S direita
+            elif numero_da_linha == TAMANHO_DA_MATRIZ-1:
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-1]) #N esquerda
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-0]) #N
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna+1]) #N direita
+                vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna-1]) #Esquerda
+                vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna+1]) #Direita
+            elif numero_da_coluna == TAMANHO_DA_MATRIZ-1:
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-1]) #N esquerda
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-0]) #N
+                vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna-1]) #Esquerda
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-1]) #S esquerda
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-0]) #S
+            # Se não:
+            else:
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-1]) #N esquerda
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna-0]) #N
+                vizinhos.append(matriz[numero_da_linha-1][numero_da_coluna+1]) #N direita
+                vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna-1]) #Esquerda
+                vizinhos.append(matriz[numero_da_linha-0][numero_da_coluna+1]) #Direita
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-1]) #S esquerda
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna-0]) #S
+                vizinhos.append(matriz[numero_da_linha+1][numero_da_coluna+1]) #S direita
 
-        # Vizinho vivos:
-        for vizinho in vizinhos:
-            if vizinho == 1:
-                numero_de_vizinhos_vivos +=1
-        
-        # Transformação:
-        if celula == 0 and numero_de_vizinhos_vivos == 3:
-            matriz[numero_da_linha][numero_da_coluna] = 1 # recebe 1
-        elif (celula == 1 and numero_de_vizinhos_vivos == 2) or (celula == 1 and numero_de_vizinhos_vivos == 3):
-            matriz[numero_da_linha][numero_da_coluna] = 1 # recebe 1
-        else:
-            matriz[numero_da_linha][numero_da_coluna] = 0 # recebe 0
+            # Vizinho vivos:
+            for vizinho in vizinhos:
+                if vizinho == 1:
+                    numero_de_vizinhos_vivos +=1
+            
+            # Transformação:
+            if celula == 0 and numero_de_vizinhos_vivos == 3:
+                nova_matriz[numero_da_linha][numero_da_coluna] = 1 # recebe 1
+            elif (celula == 1 and numero_de_vizinhos_vivos == 2) or (celula == 1 and numero_de_vizinhos_vivos == 3):
+                nova_matriz[numero_da_linha][numero_da_coluna] = 1 # recebe 1
+            else:
+                nova_matriz[numero_da_linha][numero_da_coluna] = 0 # recebe 0
 
-print('\n')
-print("tempo 1: ") # Dá pra colocar o i aqui se fizer o for comentado da linha 33...
-print(matriz)
-print('\n')
-# E botar um sleep aqui pra não aparecer tudo de uma vez.
+    matriz = copy.deepcopy(nova_matriz)
+
+    sleep(1)
+    print('\n')
+    print(f"tempo {i+1}: ")
+    print(matriz)
+    print('\n')
